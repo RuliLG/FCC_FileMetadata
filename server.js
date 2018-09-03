126ff8b5f713f45df75d09a5fe483d81a413ea92
Glitch (somber-gopher) <none>
2018-09-03 22:01:46 +0000

    🚲🍧 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var cors = require('cors');
+var multer = require('multer');
+var express = require('express');
+var app = express();
+var upload = multer({
+  storage: multer.memoryStorage()
+});
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({name: req.file.originalname, size: req.file.size, type: req.file.mimetype});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
